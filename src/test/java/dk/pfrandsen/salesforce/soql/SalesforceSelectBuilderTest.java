3f4ba6d81dd4e86f4def85e292d44a71a31b177b
Peter Frandsen <pfrandsen@gmail.com>
2018-01-14 08:39:13 +0100

    Update SalesforceSelectBuilderTest.java
    added unit tests for SalesforceSelectBuilder

--- src/test/java/dk/pfrandsen/salesforce/soql/SalesforceSelectBuilderTest.java
+++ src/test/java/dk/pfrandsen/salesforce/soql/SalesforceSelectBuilderTest.java
@@ -1,5 +1,53 @@
 package dk.pfrandsen.salesforce.soql;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 public class SalesforceSelectBuilderTest {
+
+    @Test
+    public void testNotInitializedFields() {
+        SalesforceSelectBuilder builder = new SalesforceSelectBuilder();
+        assertNull(builder.build());
+        assertNull(builder.setsObject("Account").build());
+    }
+
+    @Test
+    public void testNotInitializedSObject() {
+        SalesforceSelectBuilder builder = new SalesforceSelectBuilder();
+        assertNull(builder.build());
+        assertNull(builder.addField("xyz").build());
+        assertNull(builder.addRelationField("rel", "fld").build());
+    }
+
+    @Test
+    public void testQuery() {
+        SalesforceSelectBuilder builder = new SalesforceSelectBuilder();
+        builder.setsObject("Account").addField("fld");
+        assertEquals("select fld from Account", builder.build());
+        builder.addId("x");
+        assertEquals("select fld from Account where id = 'x'", builder.build());
+        builder.addId("y");
+        assertEquals("select fld from Account where id in ['x', 'y']", builder.build());
+    }
+
+    @Test
+    public void testRelFields() {
+        SalesforceSelectBuilder builder = new SalesforceSelectBuilder();
+        builder.setsObject("Account").addFields(Arrays.asList("f1", "f2"));
+        assertEquals("select f1, f2 from Account", builder.build());
+        builder.addRelationFields("r", Arrays.asList("f3", "f4"));
+        assertEquals("select f1, f2, r.f3, r.f4 from Account", builder.build());
+        builder.addRelationField("m", "f5");
+        assertEquals("select f1, f2, r.f3, r.f4, m.f5 from Account", builder.build());
+        builder.addId("x");
+        assertEquals("select f1, f2, r.f3, r.f4, m.f5 from Account where id = 'x'", builder.build());
+        builder.addId("y");
+        assertEquals("select f1, f2, r.f3, r.f4, m.f5 from Account where id in ['x', 'y']", builder.build());
+    }
 
 }
