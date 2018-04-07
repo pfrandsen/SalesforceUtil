50165cad3181713c770f807edc58863bd02dbcc0
pfrandsen <pfrandsen@gmail.com>
2018-04-07 18:07:52 +0200

    utility to remove/replace prefix in object keys in json tree

---
+++ src/main/java/dk/pfrandsen/salesforce/NSMap.java
@@ -0,0 +1,91 @@
+package dk.pfrandsen.salesforce;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.Iterator;
+
+public final class NSMap {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    private NSMap() {
+    }
+
+    /**
+     * Copy JSON tree while removing prefix from all object keys that starts with given prefix. If removing prefix from
+     * key results in a name clash with another key in the same object node, then the last key seen will win
+     * (other key/value is lost). In case of key clash expect unpredictable results in terms of which value is lost.
+     *
+     * @param src source json tree
+     * @param prefix prefix to remove from object keys
+     * @return copy of source tree with prefix removed from object keys
+     */
+    public static JsonNode transform(JsonNode src, String prefix) {
+        return transform(src, prefix, null);
+    }
+
+    public static JsonNode transform(JsonNode src, String prefix, String replace) {
+        if (src == null) {
+            return null;
+        }
+        if (!src.isContainerNode()) {
+            return src.deepCopy();
+        }
+        if (src.isArray()) {
+            ArrayNode arr = MAPPER.createArrayNode();
+            Iterator<JsonNode> iter = src.iterator();
+            while (iter.hasNext()) {
+                JsonNode t = transform(iter.next(), prefix, replace);
+                arr.add(t);
+            }
+            return arr;
+        }
+        if (src.isObject()) {
+            ObjectNode obj = MAPPER.createObjectNode();
+            Iterator<String> iter = src.fieldNames();
+            while (iter.hasNext()) {
+                String nodeName = iter.next();
+                JsonNode node = src.path(nodeName);
+                obj.set(map(prefix, replace, nodeName), transform(node, prefix, replace));
+            }
+            return obj;
+        }
+        // should never get here
+        return null;
+    }
+
+    /**
+     * Remove prefix from string.
+     *
+     * @param prefix prefix to remove from string
+     * @param value string to remove prefix from
+     * @return value if value is null or does not start with prefix, else value with prefix removed
+     */
+    public static String rm(String prefix, String value) {
+        if (prefix == null || prefix.length() == 0 || value == null) {
+            return null;
+        }
+        return value.startsWith(prefix) ? value.substring(prefix.length()) : value;
+    }
+
+    /**
+     * Replace prefix in string.
+     *
+     * @param prefix prefix to be replaced
+     * @param replace string to replece prefix with
+     * @param value string to replace prefix in
+     * @return value if value is null or does not start with prefix, else value with prefix replaced
+     */
+    public static String map(String prefix, String replace, String value) {
+        if (prefix == null || prefix.length() == 0 || value == null) {
+            return null;
+        }
+        if (replace == null || replace.length() == 0) {
+            return value.startsWith(prefix) ? value.substring(prefix.length()) : value;
+        }
+        return value.startsWith(prefix) ? (replace + value.substring(prefix.length())) : value;
+    }
+
+}
