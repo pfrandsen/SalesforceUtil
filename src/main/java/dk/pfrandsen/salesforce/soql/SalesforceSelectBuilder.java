ef0b1c773ead60c0fc74a862e9c668413b326773
pfrandsen <pfrandsen@gmail.com>
2018-04-08 17:19:45 +0200

    added multilevel relations; added tests and javadoc

--- src/main/java/dk/pfrandsen/salesforce/soql/SalesforceSelectBuilder.java
+++ src/main/java/dk/pfrandsen/salesforce/soql/SalesforceSelectBuilder.java
@@ -1,5 +1,6 @@
 package dk.pfrandsen.salesforce.soql;
 
+import dk.pfrandsen.salesforce.NSMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,11 +15,15 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
- * Helper class for building SOQL select queries.
+ * Helper class for building SOQL select queries. If prefix and/or replace are set the namespace prefix for
+ * sObject, fields, and relations values are removed/replaced.
  */
 public class SalesforceSelectBuilder {
     private static final Logger logger = LoggerFactory.getLogger(SalesforceSelectBuilder.class);
 
+    private String prefix; // prefix value to remove or replace
+    private String replace; // prefix replace value
+    private boolean logQuery; // if true final query is logged when build
     private String sObject;
     private final List<String> idSet;
     private final List<String> fields;
@@ -32,62 +37,191 @@ public class SalesforceSelectBuilder {
         limit = 0;
     }
 
+    public SalesforceSelectBuilder(boolean logQuery) {
+        this();
+        this.logQuery = logQuery;
+    }
+
+    public SalesforceSelectBuilder(String prefix, String replace) {
+        this();
+        this.prefix = prefix;
+        this.replace = replace;
+    }
+
+    public SalesforceSelectBuilder(String prefix, String replace, boolean logQuery) {
+        this(logQuery);
+        this.prefix = prefix;
+        this.replace = replace;
+    }
+
     public SalesforceSelectBuilder setsObject(String sObject) {
-        this.sObject = sObject;
+        this.sObject = map(sObject);
         return this;
     }
 
+    /**
+     * Add id value to query id set.
+     *
+     * @param id id value, only added if non-empty.
+     * @return
+     */
+    public SalesforceSelectBuilder addId(String id) {
+        String i = id == null ? "" : id.trim();
+        if (i.length() > 0) {
+            add(i, idSet);
+        }
+        return this;
+    }
+
+    /**
+     * Add list of id values to query id set.
+     *
+     * @param ids list of id values, values that are non-empty are added
+     * @return builder
+     */
     public SalesforceSelectBuilder addId(String... ids) {
         return addId(Arrays.asList(ids));
     }
 
-    public SalesforceSelectBuilder addId(String id) {
-        String i = id == null ? "" : id.trim();
-        if (i.length() > 0) {
-            idSet.add(i);
-        }
-        return this;
-    }
-
+    /**
+     * Add list of id values to query id set.
+     *
+     * @param ids list of id values, values that are non-empty are added
+     * @return builder
+     */
     public SalesforceSelectBuilder addId(List<String> ids) {
-        List<String> list = getNonEmpty(ids);
-        if (!list.isEmpty()) {
-            idSet.addAll(list);
-        }
+        List<String> list = ids == null ? new ArrayList<>() : getNonEmpty(ids);
+        list.forEach(id -> {
+            addId(id);
+        });
         return this;
     }
 
+    /**
+     * Add field to query. Field is only added if it is not empty/whitespace and
+     * it is not already added.
+     * @param field field name
+     * @return builder
+     */
     public SalesforceSelectBuilder addField(String field) {
         String f = field == null ? "" : field.trim();
         if (f.length() > 0) {
-            fields.add(f);
+            add(map(f), fields);
         }
         return this;
     }
 
+    /**
+     * Add list of fields to query. An entry in the list is only added if it is
+     * not empty/whitespace and it is not already added.
+     *
+     * @param fields field names
+     * @return builder
+     */
     public SalesforceSelectBuilder addFields(List<String> fields) {
         List<String> list = getNonEmpty(fields);
         if (!list.isEmpty()) {
-            this.fields.addAll(list);
+            list.forEach(this::addField);
         }
         return this;
     }
 
+    /**
+     * Add list of fields to query. An entry in the list is only added if it is
+     * not empty/whitespace and it is not already added.
+     *
+     * @param fields field names
+     * @return builder
+     */
+    public SalesforceSelectBuilder addFields(String... fields) {
+        return addFields(Arrays.asList(fields));
+    }
+
+    /**
+     * Add single level relationship (x__r.y) lookup to query.
+     *
+     * @param rel relationship name
+     * @param field field name
+     * @return builder
+     */
     public SalesforceSelectBuilder addRelationField(String rel, String field) {
         String r = rel == null ? "" : rel.trim();
         String f = field == null ? "" : field.trim();
         if (r.length() > 0 && f.length() > 0) {
-            relationFields.add(new AbstractMap.SimpleEntry<>(r, Collections.singletonList(f)));
+            addRel(new AbstractMap.SimpleEntry<>(map(r), Collections.singletonList(map(f))));
         }
         return this;
     }
 
+    /**
+     * Add multi-level relationship (x__r.y__r[...].z) lookup to query.
+     *
+     * @param rel relationship names (max 5, not checked).
+     * @param field field name
+     * @return builder
+     */
+    public SalesforceSelectBuilder addRelationField(List<String> rel, String field) {
+        String r = String.join(".", getNonEmpty(rel).stream().map(this::map).collect(Collectors.toList()));
+        String f = field == null ? "" : field.trim();
+        if (r.length() > 0 && f.length() > 0) {
+            addRel(new AbstractMap.SimpleEntry<>(r, Collections.singletonList(map(f))));
+        }
+        return this;
+    }
+
+    /**
+     * Add single level relationships (x__r.y, x__r.z, ...) lookup to query.
+     *
+     * @param rel relationship name
+     * @param fields field names
+     * @return builder
+     */
+    public SalesforceSelectBuilder addRelationFields(String rel, String... fields) {
+        return addRelationFields(rel, Arrays.asList(fields));
+    }
+
+    /**
+     * Add single level relationships (x__r.y, x__r.z, ...) lookup to query.
+     *
+     * @param rel relationship name
+     * @param fields field names
+     * @return builder
+     */
     public SalesforceSelectBuilder addRelationFields(String rel, List<String> fields) {
         String r = rel == null ? "" : rel.trim();
         if (r.length() > 0 && fields != null) {
-            List<String> f = getNonEmpty(fields);
+            List<String> f = getNonEmpty(fields).stream().map(this::map).collect(Collectors.toList());
             if (!f.isEmpty()) {
-                relationFields.add(new AbstractMap.SimpleEntry<>(r, f));
+                addRel(new AbstractMap.SimpleEntry<>(map(r), f));
+            }
+        }
+        return this;
+    }
+
+    /**
+     * Add multi-level relationships (x__r.y__r[...].a, x__r.y__r[...].b, ...) lookup to query.
+     *
+     * @param rel relationship names (max 5, not checked).
+     * @param fields field names
+     * @return builder
+     */
+    public SalesforceSelectBuilder addRelationFields(List<String> rel, String... fields) {
+        return addRelationFields(rel, Arrays.asList(fields));
+    }
+
+    /**
+     * Add multi-level relationships (x__r.y__r[...].a, x__r.y__r[...].b, ...) lookup to query.
+     *
+     * @param rel relationship names (max 5, not checked).
+     * @param fields field names
+     * @return builder
+     */
+    public SalesforceSelectBuilder addRelationFields(List<String> rel, List<String> fields) {
+        String r = String.join(".", getNonEmpty(rel).stream().map(this::map).collect(Collectors.toList()));
+        if (r.length() > 0 && fields != null) {
+            List<String> f = getNonEmpty(fields).stream().map(this::map).collect(Collectors.toList());
+            if (!f.isEmpty()) {
+                addRel(new AbstractMap.SimpleEntry<>(r, f));
             }
         }
         return this;
@@ -113,7 +247,6 @@ public class SalesforceSelectBuilder {
             logger.error("query field(s) not specified");
             return null;
         }
-
         List<String> fld = new ArrayList<>();
         fld.addAll(fields);
         fld.addAll(relationFields.stream().map(this::join).flatMap(Collection::stream).collect(Collectors.toList()));
@@ -127,6 +260,9 @@ public class SalesforceSelectBuilder {
         }
         if (limit > 0) {
             query += " limit = " + limit;
+        }
+        if (logQuery) {
+            logger.info(query);
         }
         return query;
     }
@@ -144,6 +280,7 @@ public class SalesforceSelectBuilder {
     }
 
     /**
+     * Join key with each element in list. Returns list of key.value strings.
      *
      * @param entry relation name and list of fields in relation
      * @return a list of strings where each string is the value from {@code entry} prefixed with the key
@@ -152,6 +289,56 @@ public class SalesforceSelectBuilder {
     private List<String> join(AbstractMap.SimpleEntry<String, List<String>> entry) {
         String rel = entry.getKey();
         return entry.getValue().stream().map(v -> rel + "." + v).collect(Collectors.toList());
+    }
+
+    /**
+     * Remove/replace namespace prefix in string.
+     * Only removes/replaces prefix if prefix is non-empty and value starts with prefix.
+     * Only replaces prefix if replace is non-empty string
+     *
+     * @param value string to replace prefix in / remove
+     * @return modified value or identity
+     */
+    private String map(String value) {
+        return NSMap.map(prefix, replace, value);
+    }
+
+    /**
+     * Add value to value "set" if it does not already exist in set.
+     *
+     * @param value value to add to "set"
+     * @param valueSet list to add value to (treated like an ordered set)
+     */
+    private void add(String value, List<String> valueSet) {
+        if (!valueSet.contains(value)) {
+            valueSet.add(value);
+        }
+    }
+
+    /**
+     * Add values to value "set". Only values thar does not already exist in set are added.
+     *
+     * @param values values to add to "set"
+     * @param valueSet list to add value to (treated like an ordered set)
+     */
+    private void add(List<String> values, List<String> valueSet) {
+        values.forEach(v -> {
+            add(v, valueSet);
+        });
+    }
+
+    private void addRel(AbstractMap.SimpleEntry<String, List<String>> entry) {
+        List<String> ex = relationFields.stream().filter(e -> e.getKey().equals(entry.getKey()))
+                .map(AbstractMap.SimpleEntry::getValue).findFirst().orElse(null);
+        if (ex == null) {
+            relationFields.add(entry);
+        } else {
+            entry.getValue().forEach(v -> {
+                if (!ex.contains(v)) {
+                    ex.add(v);
+                }
+            });
+        }
     }
 
 }
--- src/test/java/dk/pfrandsen/salesforce/soql/SalesforceSelectBuilderTest.java
+++ src/test/java/dk/pfrandsen/salesforce/soql/SalesforceSelectBuilderTest.java
@@ -31,8 +31,30 @@ public class SalesforceSelectBuilderTest {
         assertEquals("select fld from Account", builder.build());
         builder.addId("x");
         assertEquals("select fld from Account where id = 'x'", builder.build());
-        builder.addId("y", "z");
+        builder.addId("y", "z", " y ", "z", " x");
         assertEquals("select fld from Account where id in ['x', 'y', 'z']", builder.build());
+    }
+
+    @Test
+    public void testQueryRemovePrefix() {
+        SalesforceSelectBuilder builder = new SalesforceSelectBuilder("Pre_", null);
+        builder.setsObject("Pre_Account").addField("Pre_fld");
+        assertEquals("select fld from Account", builder.build());
+        // prefix not removed from id's
+        builder.addId("Pre_x");
+        assertEquals("select fld from Account where id = 'Pre_x'", builder.build());
+        builder.addId("Pre_y", "z");
+        assertEquals("select fld from Account where id in ['Pre_x', 'Pre_y', 'z']", builder.build());
+    }
+
+    @Test
+    public void testQueryReplacePrefix() {
+        SalesforceSelectBuilder builder = new SalesforceSelectBuilder("P_", "R_");
+        builder.setsObject("P_Account").addField("P_fld");
+        assertEquals("select R_fld from R_Account", builder.build());
+        // prefix not removed from id's
+        builder.addId("P_x");
+        assertEquals("select R_fld from R_Account where id = 'P_x'", builder.build());
     }
 
     @Test
@@ -48,6 +70,56 @@ public class SalesforceSelectBuilderTest {
         assertEquals("select f1, f2, r.f3, r.f4, m.f5 from Account where id = 'x'", builder.build());
         builder.addId("y");
         assertEquals("select f1, f2, r.f3, r.f4, m.f5 from Account where id in ['x', 'y']", builder.build());
+    }
+
+    @Test
+    public void testRelFieldsRepeatValue() {
+        SalesforceSelectBuilder builder = new SalesforceSelectBuilder();
+        builder.setsObject("Account");
+        builder.addRelationFields("r", Arrays.asList("f1", "f2"));
+        assertEquals("select r.f1, r.f2 from Account", builder.build());
+        builder.addRelationFields("r", Arrays.asList("f2"));
+        assertEquals("select r.f1, r.f2 from Account", builder.build());
+        builder.addRelationFields("rr", Arrays.asList("fx"));
+        assertEquals("select r.f1, r.f2, rr.fx from Account", builder.build());
+        builder.addRelationFields("r", Arrays.asList("fx"));
+        assertEquals("select r.f1, r.f2, r.fx, rr.fx from Account", builder.build());
+    }
+
+    @Test
+    public void testRelFieldsMultilevel() {
+        SalesforceSelectBuilder builder = new SalesforceSelectBuilder();
+        builder.setsObject("p_Custom");
+        builder.addRelationFields("p_rx", "f1", "f2");
+        assertEquals("select p_rx.f1, p_rx.f2 from p_Custom", builder.build());
+        builder.addRelationFields(Arrays.asList("p_ry", "p_rz"), "f2", "f1");
+        assertEquals("select p_rx.f1, p_rx.f2, p_ry.p_rz.f2, p_ry.p_rz.f1 from p_Custom", builder.build());
+        builder.addRelationFields(Arrays.asList("p_ry", "p_rz"), Arrays.asList("f2", "fx"));
+        assertEquals("select p_rx.f1, p_rx.f2, p_ry.p_rz.f2, p_ry.p_rz.f1, p_ry.p_rz.fx from p_Custom", builder.build());
+    }
+
+    @Test
+    public void testRelFieldsMultilevelNSReplace() {
+        SalesforceSelectBuilder builder = new SalesforceSelectBuilder("p_", "o__");
+        builder.setsObject("p_Custom").addField("p_fy");
+        builder.addRelationFields("p_rx", "f1", "f2");
+        assertEquals("select o__fy, o__rx.f1, o__rx.f2 from o__Custom", builder.build());
+        builder.addRelationFields(Arrays.asList("p_ry", "p_rz"), "f2", "f1");
+        assertEquals("select o__fy, o__rx.f1, o__rx.f2, o__ry.o__rz.f2, o__ry.o__rz.f1 from o__Custom", builder.build());
+        builder.addRelationFields(Arrays.asList("p_ry", "p_rz"), Arrays.asList("f2", "fx"));
+        assertEquals("select o__fy, o__rx.f1, o__rx.f2, o__ry.o__rz.f2, o__ry.o__rz.f1, o__ry.o__rz.fx from o__Custom", builder.build());
+    }
+
+    @Test
+    public void testRelFieldsMultilevelNSRemove() {
+        SalesforceSelectBuilder builder = new SalesforceSelectBuilder("p_", null);
+        builder.setsObject("p_Custom").addField("p_fy");
+        builder.addRelationFields("p_rx", "f1", "f2");
+        assertEquals("select fy, rx.f1, rx.f2 from Custom", builder.build());
+        builder.addRelationFields(Arrays.asList("p_ry", "p_rz"), "f2", "f1");
+        assertEquals("select fy, rx.f1, rx.f2, ry.rz.f2, ry.rz.f1 from Custom", builder.build());
+        builder.addRelationFields(Arrays.asList("p_ry", "p_rz"), Arrays.asList("f2", "fx"));
+        assertEquals("select fy, rx.f1, rx.f2, ry.rz.f2, ry.rz.f1, ry.rz.fx from Custom", builder.build());
     }
 
     @Test
